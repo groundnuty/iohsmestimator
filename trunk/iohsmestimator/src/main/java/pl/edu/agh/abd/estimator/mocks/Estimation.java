bd79f0739d6ae27a6bc960f5c59e2ca8d66f1e1f
orzechowski.michal@gmail.com <orzechowski.michal@gmail.com@7fb9cd66-275a-a2b7-706b-145f090b1f30>
2011-01-07 05:04:50 +0000

    embended estimation alghoritm into project and added all needed mock classes so that 1) the code would compile 2) it was easy to see what information we will have to supply to estimation algorithm to make it work

---
+++ trunk/iohsmestimator/src/main/java/pl/edu/agh/abd/estimator/mocks/Estimation.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.abd.estimator.mocks;
+
+/**
+ * created at: Jan 6, 2011, 11:58:44 PM
+ *
+ * @author: Michal Orzechowski
+ */
+public class Estimation {
+    public Estimation(float bandwidth, float latency, int fileSize) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ trunk/iohsmestimator/src/main/java/pl/edu/agh/abd/estimator/mocks/Tape.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.abd.estimator.mocks;
+
+/**
+ * created at: Jan 6, 2011, 11:55:45 PM
+ *
+ * @author: Michal Orzechowski
+ */
+public class Tape {
+    public String getTapeID() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public String getMediaType() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}

8b3a3c2e8d272d9e0cbc77394a8d9c70ec1658ed
lucas.suder <lucas.suder@7fb9cd66-275a-a2b7-706b-145f090b1f30>
2011-01-15 17:24:11 +0000

    fixed test

--- trunk/iohsmestimator/src/main/test/ServiceTest.java
+++ trunk/iohsmestimator/src/main/test/ServiceTest.java
@@ -7,6 +7,12 @@ import pl.edu.agh.abd.estimator.mocks.HSMFileInfo;
 
 public class ServiceTest  extends TestCase{
 
+	private static final boolean FILE_TAPE_IN_DRIVE = false;
+	private static final String FILE_MEDIA_TYPE = "tape";
+	private static final boolean FILE_IS_CACHED = false;
+	private static final int FILE_START_BLOCK = 12;
+	private static final int FILE_END_BLOCK = 10012;
+	private static final String FILE_TAPE_ID = "1";
 	private static final boolean ANY_EMPTY_DRIVE = false;
 	private static final double HSM_SYSTEM_TRANSFER_RATE = 100.0;
 	private static final double HSM_UNLOAD_TAPE_LATENCY = 10.0;
@@ -18,14 +24,14 @@ public class ServiceTest  extends TestCase{
 	private static final String FILE_NAME = "noname";
 	
 	private static final boolean SECOND_IS_TYPE_WITH_FILE_IN_DRIVE = true;
-	private static final String SECOND_MEDIA_TYPE = "tape";
+	private static final String SECOND_MEDIA_TYPE = FILE_MEDIA_TYPE;
 	private static final boolean SECOND_IS_CACHED = false;
 	private static final int SECOND_START_BLOCK = 12;
 	private static final int SECOND_END_BLOCK = 10012;
 	private static final String SECOND_TYPE_ID = "3";
 	
 	private static final boolean FIRST_IS_TYPE_WITH_FILE_IN_DRIVE = false;
-	private static final String FIRST_MEDIA_TYPE = "tape";
+	private static final String FIRST_MEDIA_TYPE = FILE_MEDIA_TYPE;
 	private static final boolean FIRST_IS_CACHED = true;
 	private static final int FIRST_START_BLOCK = 12;
 	private static final int FIRST_END_BLOCK = 10012;
@@ -78,13 +84,13 @@ public class ServiceTest  extends TestCase{
 		HSMMonitoringStub stub = new HSMMonitoringStub();
 		
 		HSMFileInfo file = stub.getHSMFileInfo(FILE_NAME);
-		
-		assertEquals(FIRST_TYPE_ID, file.getTapeID());
-		assertEquals(FIRST_END_BLOCK, file.getEndBlock());
-		assertEquals(FIRST_START_BLOCK, file.getStartBlock());
-		assertEquals(FIRST_IS_CACHED, file.isIsCached());
-		assertEquals(FIRST_MEDIA_TYPE, file.getMediaType());
-		assertEquals(FIRST_IS_TYPE_WITH_FILE_IN_DRIVE, file.isTapeWithFileInDrive());
+
+		assertEquals(FILE_TAPE_ID, file.getTapeID());
+		assertEquals(FILE_END_BLOCK, file.getEndBlock());
+		assertEquals(FILE_START_BLOCK, file.getStartBlock());
+		assertEquals(FILE_IS_CACHED, file.isIsCached());
+		assertEquals(FILE_MEDIA_TYPE, file.getMediaType());
+		assertEquals(FILE_TAPE_IN_DRIVE, file.isTapeWithFileInDrive());
 	}
 	
 	@Test

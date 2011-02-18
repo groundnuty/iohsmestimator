e8107c8171b348a103b52db4641bd2f9a1aa5afe
lucas.suder <lucas.suder@7fb9cd66-275a-a2b7-706b-145f090b1f30>
2011-02-18 19:42:53 +0000

    porting part 1.

---
+++ trunk/serverStub/src/main/java/pl/edu/agh/storage/estimation/hsmclient/HSMFileWrapper.java
@@ -0,0 +1,5 @@
+package pl.edu.agh.storage.estimation.hsmclient;
+
+public class HSMFileWrapper {
+	public HSMFile hsmFile = new HSMFile();
+}
---
+++ trunk/serverStub/src/main/java/pl/edu/agh/storage/estimation/hsmclient/MonitoringConstants.java
@@ -0,0 +1,36 @@
+package pl.edu.agh.storage.estimation.hsmclient;
+
+
+/**
+ * Constant values for the storage monitoring library.
+ * 
+ * @author Kornel Skalkowski
+ */
+public class MonitoringConstants {
+
+	/**
+	 * Default value for unavailable integer parameters.
+	 */
+	public final static int UNAVAILABLE_VALUE = 0;
+
+	/**
+	 * Default value for unavailable string parameters.
+	 */
+	public final static String UNAVAILABLE_STRING_VALUE = "unknown";
+
+	/**
+	 * The configuration file name.
+	 */
+	public final static String PROPERTIES_FILE = "storage.properties";
+
+	/**
+	 * The config directory name.
+	 */
+	public final static String CONFIG_DIR = "settings";
+
+	/**
+	 * The resources directory name.
+	 */
+	public final static String RESOURCES_DIR = "resources";
+
+}
